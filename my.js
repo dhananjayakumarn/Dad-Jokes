bf7c5dcc41c1c793b7b4d95ff9cb9e3ac15e1880
dhananjayakumarn <74400182+dhananjayakumarn@users.noreply.github.com>
2022-05-05 19:45:22 +0530

    Add files via upload

---
+++ my.js
@@ -0,0 +1,23 @@
+joke = document.getElementById('joke');
+bt = document.getElementById('bt');
+
+bt.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  joke.innerHTML = data.joke
+}
+
+
+
